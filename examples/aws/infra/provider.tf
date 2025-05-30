2449977f41b17c02487a84844c276dd27961f675
estanqueiroa <50240388+estanqueiroa@users.noreply.github.com>
2025-05-30 10:48:50 -0300

    Update provider.tf

--- examples/aws/infra/provider.tf
+++ examples/aws/infra/provider.tf
@@ -1,15 +1,13 @@
+## Specifies the Region your Terraform Provider will server
 provider "aws" {
-  region                      = var.region
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true
-
-  endpoints {
-    dynamodb = "http://localstack:4566"
-    lambda   = "http://localstack:4566"
-    s3       = "http://localstack:4566"
-    sqs      = "http://localstack:4566"
-    sns      = "http://localstack:4566"
+  region = var.region
+  default_tags {
+    tags = {
+      Environment = "Test22"
+      Owner       = "estanqua"
+      Project     = "SrcAod2025"
+      IAC         = "Terraform"
+      Path        = "/aws/2sp-data/data-processing-demo/src/aodsrc2025/"
+    }
   }
 }
